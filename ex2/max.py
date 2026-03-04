c15c14ef18d010e49e2ed716e510bf761525a603
Antony-1978 <antonypraveen2246@gmail.com>
2026-03-04 23:43:05 +0530

    added max.py

---
+++ ex2/max.py
@@ -0,0 +1,9 @@
+a=int(input("enter the value:"))
+b=int(input("enter the value:"))
+c=int(input("enter the value:"))
+if (a>b and a>c):
+   print("the maximum:",a)
+elif (b>a and b>c):
+   print("the maximum:",b)
+else:
+   print("the maximum:",c)
